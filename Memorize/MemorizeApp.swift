bcf6a6c3f5ad08880683503ac61b0658c5f19fd4
DjNovovic <djordjijenovovic@gmail.com>
2020-12-20 11:49:53 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Đorđije Novović on 20.12.20..
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
